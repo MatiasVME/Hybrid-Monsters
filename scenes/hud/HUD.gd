4b0fd1771436484ee699c28533597c6dd6559400
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-11-10 19:59:04 -0300

    Menu

--- scenes/hud/HUD.gd
+++ scenes/hud/HUD.gd
@@ -5,3 +5,12 @@ func _on_HUDInventory_toggled(button_pressed):
 		$AnimInv.play("show")
 	else:
 		$AnimInv.play("hide")
+
+func _on_HUDMenuButton_pressed():
+	$AnimMenu.play("show")
+
+func _on_Resume_pressed():
+	$AnimMenu.play("hide")
+
+func _on_Menu_pressed():
+	get_tree().change_scene("res://scenes/main_screens/MainMenu.tscn")
